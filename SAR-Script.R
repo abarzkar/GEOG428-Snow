276ad8079b780f0d5e71fd39d7316281090141d9
SebYerex <142267373+SebYerex@users.noreply.github.com>
2024-03-07 16:10:39 -0800

    Update SAR-Script.R

--- SAR-Script.R
+++ SAR-Script.R
@@ -14,20 +14,33 @@ lidar.SD <- rast("D:/422/LiDAR_derived_snow_depth_ACO_CHRL_2021_P1.tif")
 p <- vect("D:/422/SAR-Points2.shp")
 
 #extract the sAR values from under the points
-backscatter<- as.data.frame(extract(vv.vh, p))
+backscatter<- as.data.frame(extract(vv.vh, p, cells=TRUE, xy=TRUE))
 
 #resample the LiDAR snow depth to the res of the SAR data (10m)
 r <- resample(lidar.SD, ReSamp)
 
 #extract the snow depth values from under the sAR pixels via the points
-snowdepth <- as.data.frame(extract(r, p))
-names(snowdepth) <- c("ID", "snowdepth")
+snowdepth <- as.data.frame(extract(r, p, cells=TRUE, xy=TRUE))
+
 
 
 #join the backscatter and snow depth dataframes
 SAR.Snow <- bind_cols(backscatter, snowdepth)
-names(SAR.Snow) <- c("ID1", "VV_VH", "ID2", "SnowDepth")
+SAR.Snow <- merge(backscatter, snowdepth, by=c("x", "y"))
 
-SAR.Snow <- select(SAR.Snow, c("VV_VH", "SnowDepth"))
+SAR.Snow <- select(SAR.Snow, c("VH_VV", "LiDAR_derived_snow_depth_ACO_CHRL_2021_P1"))
+names(SAR.Snow) <- c("VH_VV", "SnowDepth")
 
+plot(SAR.Snow$VH_VV, SAR.Snow$SnowDepth)
 SAR.Snow <- na.omit(SAR.Snow)
+SAR.Snow <- subset(SAR.Snow, SnowDepth > 0)
+
+Snow10.20 <- subset(SAR.Snow, SnowDepth < 0.5)
+plot(Snow10.20$VH_VV)
+plot(Snow10.20$SnowDepth)
+
+Snow10.20$VH_VV <- Snow10.20$VH_VV/(max(Snow10.20$VH_VV))
+Snow10.20$SnowDepth <- Snow10.20$SnowDepth/(max(Snow10.20$SnowDepth))
+
+T<- lm(VH_VV ~ SnowDepth, Snow10.20)
+plot(Snow10.20)
